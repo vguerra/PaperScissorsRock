2a7bdefbbf6ac8824459a4eb91e3d8338b1432d7
Victor Guerra <vguerra@gmail.com>
2015-03-25 10:02:27 +0100

    adding image sets for each of the actions and adding a view controller to present the game results.

---
+++ Roshambo/ResultsController.swift
@@ -0,0 +1,16 @@
+//
+//  ResultsController.swift
+//  Roshambo
+//
+//  Created by Victor Guerra on 25/03/15.
+//  Copyright (c) 2015 Victor Guerra. All rights reserved.
+//
+
+import UIKit
+
+class ResultsController: UIViewController {
+
+    @IBAction func dismissView() {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
